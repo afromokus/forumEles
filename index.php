42b21ec196320ec33465d7a3f0fb320368e67802
wot14 <wot14@citromail.hu>
2018-12-10 12:58:57 +0100

    egy bug még mindig

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html class="jumbotron">
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"/>
+        <link rel="stylesheet" href="3DLabCasc.css"/>
+        
+    </head>
+    <body>
+        <div class ='kozepre'>
+            <div class="page-header jumbotron">
+                    <h1 class="beljebbKezdes">
+                        <font class="felkoverSerif">
+                        Bejelentkezés
+                        </font>
+                    </h1>
+            </div>
+            <form class="jumbotron urlap">
+                <div id="formElemek" class="beljebbKezdes">
+                    <input class="kitoltendoMezo" type='text' placeholder="felhasználói név">
+                </div>
+            </form>
+        </div>
+        <?php
+        
+        ?>
+        <script>
+            
+        </script>
+    </body>
+</html>
