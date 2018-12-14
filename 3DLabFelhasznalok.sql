e1b8b4c3712cd362645b1ffd2ecb6d2bc7c9e1b3
unknown <kirschjanosmark@gmail.com>
2018-12-14 18:53:34 +0100

    A csatlakozás sikeres, 'szoftverf' felhasználónak a jogok biztosítva.

--- 3DLabFelhasznalok.sql
+++ 3DLabFelhasznalok.sql
@@ -9,4 +9,8 @@ VALUES
 (3, 'baro', 'baro123', 'balint.robert@vasvari.hu', 3)
 ;
 
+CREATE USER 'szoftverf'@'localhost' IDENTIFIED VIA mysql_native_password USING '***';GRANT USAGE ON *.* TO 'szoftverf'@'localhost' REQUIRE NONE WITH MAX_QUERIES_PER_HOUR 0 MAX_CONNECTIONS_PER_HOUR 0 MAX_UPDATES_PER_HOUR 0 MAX_USER_CONNECTIONS 0;
+
+GRANT ALL PRIVILEGES ON 3dlab.* To 'szoftverf'@'localhost' IDENTIFIED BY 'szoftverf';
+
 
--- csatlakozas.php
+++ csatlakozas.php
@@ -1,3 +1,10 @@
 <?php
-
+    $csatlakozas = new mysqli('127.0.0.1', 'szoftverf', 'szoftverf', '3DLab', '3306');
+    
+    if($csatlakozas -> errno)
+        {
+        die('Sikertelen csatlakozás');
+        }
+        
+    //echo 'A csatlakozás sikeres!';
 ?>
