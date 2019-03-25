a25ee770ccbe1c8279c607f1837524b9aab9cb61
wot14 <wot14@citromail.hu>
2019-03-25 11:35:37 +0100

    update után sorok megszámolása

--- jelszoFrissites.php
+++ jelszoFrissites.php
@@ -8,11 +8,12 @@ if(isset($_POST["felhNev"]) && isset($_POST["ujJelszo"]) && isset($_POST["regiJe
 	$regiJelszo = $_POST["regiJelszo"];
 
 
-	$sql = 'UPDATE accountok SET jelszo = "'.$ujJelszo.'" WHERE jelszo = "'.$regiJelszo.'" AND felhNev = "'.$_POST["felhNev"]'";';
+	$sql = 'UPDATE accountok SET jelszo = "'.$ujJelszo.'" WHERE jelszo = "'.$regiJelszo.'" AND felhNev = "'.$_POST["felhNev"].'"';
+	
 
 	$csatlakozas -> query($sql);	
 }
 
-	echo ($_POST["felhNev"]." ".$sql);
+	echo ($csatlakozas -> affected_rows);
 
 ?>
